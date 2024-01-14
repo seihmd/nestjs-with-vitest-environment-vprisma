de36b8a2eb2d7f52b1f76bae0d78fe57d8e54682
seihmd <7434823+seihmd@users.noreply.github.com>
2024-01-14 15:06:35 +0900

    use fastify

---
+++ src/app.e2e.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+
+describe('AppController (e2e)', () => {
+  let app: NestFastifyApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
+
+    await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+  });
+
+  it('/ (GET)', () => {
+    return app
+      .inject({
+        method: 'GET',
+        url: '/',
+      })
+      .then((result) => {
+        expect(result.statusCode).toEqual(200);
+      });
+  });
+});
