cb299c8503b9f3b4f6fadb230ce8423ccb8e7802
seihmd <7434823+seihmd@users.noreply.github.com>
2024-01-14 15:13:41 +0900

    use prisma in controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +9,17 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, PrismaService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('root', async () => {
+    it('should return "Hello World!"', async () => {
+
+
+      expect(await appController.getHello()).toBe('Hello World!');
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService
+  ) { }
 
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
+    await this.prisma.user.create({
+      data: {
+        email: `${randomUUID()}@example.com`,
+        name: 'foo',
+      }
+    })
+
     return this.appService.getHello();
   }
 }
